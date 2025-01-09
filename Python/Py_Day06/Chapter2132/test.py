7b2fded38185b46f50c5ec5fd84d37597c8569ee
uuuj0821 <uuuj0821@naver.com>
2025-01-10 01:15:42 +0900

    python day06 chapter0132  done

--- Python/Py_Day06/Chapter2132/test.py
+++ Python/Py_Day06/Chapter2132/test.py
@@ -1,8 +1,6 @@
-# def add(num1: int, num2: int) -> str:
-#     return num1 + num2
+def add(num1: int, num2: int) -> str:
+    return num1 + num2
 
-# a = add(3.0, 4)
-# print(type(a))
-
-
-sum([1, 2])
+a = add(3.0, 4)
+print(type(a))
+ 
